ff343127c642a7e6889c568535773614807a2f2c
shohei-yamagiwa <181123035+shohei-yamagiwa@users.noreply.github.com>
2026-07-20 18:25:53 +0900

    refactor: update Class to be immutable

--- src/main/java/dev/shoheiyamagiwa/enumerator/model/algebra/Class.java
+++ src/main/java/dev/shoheiyamagiwa/enumerator/model/algebra/Class.java
@@ -1,22 +1,22 @@
 package dev.shoheiyamagiwa.enumerator.model.algebra;
 
 public class Class {
-    private String name;
+    private final String name;
 
     public Class(final String name) {
-        setName(name);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(final String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Class name cannot be null or blank!");
         }
 
         this.name = name;
+    }
+
+    public Class updateName(final String name) {
+        return new Class(name);
+    }
+
+    public String getName() {
+        return name;
     }
 
     @Override
